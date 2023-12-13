fcad95fdde6d6296c1dd54d562080075f01c2aa1
Reza Nozari <>
2023-12-13 21:57:11 +0330

    implement Front-end project
    -  conect front-end project to CouponAPIService
    -   for generate backend api I use NSwaggerStudio

---
+++ MicroservicesArtuchecture.Web/Controllers/CouponController.cs
@@ -0,0 +1,34 @@
+﻿using MicroservicesArtuchecture.Web.Services.CouponAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesArtuchecture.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        public async  Task<IActionResult> Index()
+        {
+            var client = new Client("http://localhost:5000",new HttpClient());
+
+
+            var result = await client.CouponAPIGETAsync();
+
+
+            return View(result);
+        }
+
+        [HttpGet("CreateCoupon")]
+        public async Task<IActionResult> CreateCoupon()
+        {
+            return View();
+        }
+        [HttpPost("CreateCoupon")]
+        public async Task<IActionResult> CreateCoupon(CouponContract model)
+        {
+            var client = new Client("http://localhost:5000", new HttpClient());
+
+            var result = await client.CouponAPIPOSTAsync(model);
+
+            return View(new CouponContract());
+        }
+    }
+}
---
+++ MicroservicesArtuchecture.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+
+namespace MicroservicesArtuchecture.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
+
